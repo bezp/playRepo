599224ed3bf3ab45deda7657360a2e950650f152
bezp <bezalelp@uci.edu>
2018-08-17 11:42:39 -0400

    dont merge. testing

--- src/BeckonedPath.Data/Class1.cs
+++ src/BeckonedPath.Data/Class1.cs
@@ -7,5 +7,5 @@ namespace BeckonedPath.Data
     }
 }
 
-
+//lol
 //Server=tcp:project-2.database.windows.net,1433;Initial Catalog = PlayDB; Persist Security Info=False;User ID = sqladmin; Password=Revature1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;
